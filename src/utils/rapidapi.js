64c7092c074edc26f8d7fa0b4bc6b4a66d887a6d
Madhu <nmadhu1995@gmail.com>
2024-12-16 15:11:10 +0800

    YT V1

--- src/utils/rapidapi.js
+++ src/utils/rapidapi.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
-const BASE_URL = "https://youtube138.p.rapidapi.com"
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-	headers: {
-		'x-rapidapi-key': API_KEY,
-		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
-	}
+    headers: {
+        "x-rapidapi-key": API_KEY,
+        "x-rapidapi-host": "youtube138.p.rapidapi.com",
+    },
 };
 
-
-export const fetchData = async(url) => {
+export const fetchData = async (url) => {
     try {
-        const {data} = await axios.get(`${BASE_URL}/${url}`,options)
-        return data
+        const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+        return data;
     } catch (error) {
         console.error("Error fetching api data: ", error);
-        throw error
+        throw error;
     }
-}
+};
