9d7323bb3fa5ee504aafbb719fa1416865aecd93
Madhu <nmadhu1995@gmail.com>
2024-12-16 15:26:26 +0800

    require to import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin')
+// const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        '.scrollbar-hide': {
-          /* IE and Edge */
-          '-ms-overflow-style': 'none',
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                ".scrollbar-hide": {
+                    /* IE and Edge */
+                    "-ms-overflow-style": "none",
 
-          /* Firefox */
-          'scrollbar-width': 'none',
+                    /* Firefox */
+                    "scrollbar-width": "none",
 
-          /* Safari and Chrome */
-          '&::-webkit-scrollbar': {
-            display: 'none'
-          }
-        }
-      }
-      )
-    })
-  ],
-
-  
-}
-
+                    /* Safari and Chrome */
+                    "&::-webkit-scrollbar": {
+                        display: "none",
+                    },
+                },
+            });
+        }),
+    ],
+};
